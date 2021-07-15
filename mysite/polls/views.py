ff25cbf6c0c631c2419802e0dce7f84ef13670f6
Dmitry Vasilev <vasilevdimka@mail.ru>
2021-07-16 00:54:59 +0400

    add urls and views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,24 @@
 from django.http import HttpResponse
 
-def index(requset):
-    return HttpResponse("Hello world!")
+from .models import Questions, Choice
+from polls.models import Choice
+#from mysite.polls.models import Questions
+
+
+def index(request):
+    latest_question_list = Questions.objects.order_by('-pub_date')[:5]
+    output = ','.join([q.question_text for q in latest_question_list]) # переборка всех 5 вопросов в цикле
+    return HttpResponse(output)
+
+
+def detail(request, question_id):
+    return HttpResponse("You're looking at question %s" % question_id)
+
+
+def results(request, question_id):
+    response = "You're looking at question %s" % question_id
+    return HttpResponse(response)
+
+
+def vote(request, question_id):
+    return HttpResponse("You're looking at question %s" % question_id)
