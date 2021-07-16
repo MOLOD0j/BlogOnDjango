042d36e9ea34d83339b0d6b57f3ba72345cf61b3
Dmitry Vasilev <vasilevdimka@mail.ru>
2021-07-16 16:56:29 +0400

    add HTML and update views

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,24 +1,47 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, Http404, HttpResponseRedirect
+from django.shortcuts import render, get_object_or_404
+from django.template import loader
+from django.urls import reverse
 
 from .models import Questions, Choice
-from polls.models import Choice
-#from mysite.polls.models import Questions
+#from polls.models import Choice
+
+
+# from mysite.polls.models import Questions
 
 
 def index(request):
     latest_question_list = Questions.objects.order_by('-pub_date')[:5]
-    output = ','.join([q.question_text for q in latest_question_list]) # переборка всех 5 вопросов в цикле
-    return HttpResponse(output)
+    context = {
+        'latest_question_list': latest_question_list
+    }
+    return render(request, 'polls/index.html', context)
 
 
 def detail(request, question_id):
-    return HttpResponse("You're looking at question %s" % question_id)
+    question = get_object_or_404(Questions, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
 
 
 def results(request, question_id):
-    response = "You're looking at question %s" % question_id
-    return HttpResponse(response)
+    question = get_object_or_404(Questions, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
 
 
 def vote(request, question_id):
-    return HttpResponse("You're looking at question %s" % question_id)
+    question = get_object_or_404(Questions, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Повторно отобразите форму голосования на вопрос
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Всегда возвращайте HttpResponseRedirect после успешной обработки
+        # данных POST. Это предотвращает публикацию данных дважды, если
+        # пользователь нажимает кнопку «Назад».
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
